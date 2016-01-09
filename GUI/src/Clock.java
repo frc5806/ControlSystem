34628e0575d991c6cefa92e553d65cd7ac27f5cb
truell20 <truell20@gmail.com>
2016-01-09 18:30:54 -0500

    Clock Class Code Review

--- GUI/src/Clock.java
+++ GUI/src/Clock.java
@@ -1,10 +1,12 @@
+import java.time.LocalTime;
+
 import javax.swing.*;
+
 /*
  * Clock is a panel which displays the time passed, time remaining,
  * and current stage of the competition.
  */
-public class Clock extends JPanel
-		implements Runnable
+public class Clock extends JPanel implements Runnable
 {
 	private int totalSeconds = 0;
 	private JLabel time;
@@ -17,33 +19,22 @@ public class Clock extends JPanel
 		t.start();
 	}
 	
-	private String getState() {
-		if (totalSeconds <= 15) {
-			return " State: Autonomous";
-		}
-		
-		return " State: Tele Op";
-	}
-	
-	private String addZero(int secs) {
-		if (secs < 10) {
-			return ("0" + Integer.toString(secs));
-		}
-		return Integer.toString(secs);
-	}
-	
 	public void run() {
 		long pastTime = System.currentTimeMillis();
 		while (totalSeconds <= 150) {
 			long current = System.currentTimeMillis();
 			if ((current - pastTime) >= 1000) {
 				pastTime = current;
-				time.setText(" Remaining: " +((150-totalSeconds)/60) +":" +addZero((150-totalSeconds)%60) + " Passed: " +(totalSeconds/60) +":" +addZero(totalSeconds%60) +getState() );
+				
+				String remainingTime = LocalTime.ofSecondOfDay(150-totalSeconds).toString();
+				String passedTime = LocalTime.ofSecondOfDay(totalSeconds).toString();
+				String gameState = totalSeconds <= 15 ? " State: Autonomous" : " State: Tele Op";
+				time.setText(" Remaining: " + remainingTime + " Passed: " + passedTime + gameState);
+				
 				totalSeconds++;
-				//repaint();
 			}
 		}
-		time.setText("FINISHED!!!");
+		time.setText("Game Over");
 	}
 
 }
