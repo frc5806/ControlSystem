320db103f56eb2e252b7a7bd87b75f7005b80382
Michael Truell <michael_truell@horacemann.org>
2016-01-27 17:41:14 -0500

    Took out encoder testing

--- Tank Drive/src/org/usfirst/frc/team5806/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5806/robot/Robot.java
@@ -10,19 +10,15 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class Robot extends IterativeRobot {
 	RobotDrive robot;
 	Joystick joystick;
-	
-	Encoder encoder;
     
     public void robotInit() {
          robot = new RobotDrive(0, 1);
          joystick = new Joystick(1);
-         encoder = new Encoder(0, 1);
     }
     
     public void testPeriodic() {
         LiveWindow.run();
         robot.tankDrive(joystick.getRawAxis(1), joystick.getRawAxis(5));
-        System.out.println("Distance: " + encoder.getDistance());
    }
     
     public void autonomousInit() {}
