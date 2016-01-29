c84d28c0b4711c8561e0f0dd69d4d0a9fee68ece
Michael Truell <michael_truell@horacemann.org>
2016-01-29 16:49:04 -0500

    Added encoders

--- Tank Drive/src/org/usfirst/frc/team5806/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5806/robot/Robot.java
@@ -10,19 +10,36 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class Robot extends IterativeRobot {
 	RobotDrive robot;
 	Joystick joystick;
+	Encoder[] encoders;
+	
+	// HAS TO BE A NEGATIVE NUMBER SO IT GOES THE RIGHT WAY
+	private static final double DAMPENING_COEFFICIENT = -0.75;
     
     public void robotInit() {
-         robot = new RobotDrive(0, 1);
+         robot = new RobotDrive(1, 0);
          joystick = new Joystick(1);
+         
+         encoders = new Encoder[2];
+         encoders[0] = new Encoder(0, 1);
+         encoders[0].reset();
+         encoders[1] = new Encoder(2, 3);
+         encoders[1].reset();
     }
     
     public void testPeriodic() {
         LiveWindow.run();
-        robot.tankDrive(joystick.getRawAxis(1), joystick.getRawAxis(5));
+        
+        System.out.println("encoders: " + encoders[0].getDistance() + "\t" + encoders[1].getDistance());
+        
+        teleopPeriodic();
    }
     
     public void autonomousInit() {}
     public void autonomousPeriodic() {}
     public void teleopInit() {}
-    public void teleopPeriodic() {}
+    
+    public void teleopPeriodic() {
+    	// For Xbox Controller
+    	robot.tankDrive(DAMPENING_COEFFICIENT*joystick.getRawAxis(1), DAMPENING_COEFFICIENT*joystick.getRawAxis(5), true);
+    }
 }
