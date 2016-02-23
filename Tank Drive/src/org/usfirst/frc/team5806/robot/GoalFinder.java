57c22406da00839d7a2f287d4c69de15de80dcf8
njk345 <njk@Nicks-MacBook-Retina.local>
2016-02-23 12:49:58 -0500

    Started Updating GoalFinder

--- Tank Drive/src/org/usfirst/frc/team5806/robot/GoalFinder.java
+++ Tank Drive/src/org/usfirst/frc/team5806/robot/GoalFinder.java
@@ -1,32 +1,48 @@
 package org.usfirst.frc.team5806.robot;
 
-import java.io.IOException;
+import com.ni.vision.NIVision;
 
+/*
+import java.io.IOException;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+*/
+
+//use NIVision classes for vision processing, since getImage() function
+//of USBCamera return an NIImage
 
 public class GoalFinder {
-	private NetworkTable table;
-	private static final double[] defaultValue = {-20.0, -20.0, -20.0};
-	
 	public GoalFinder() {
-		/*table = NetworkTable.getTable("grip");
+		
+	}
+	public void resizeImage(NIVision.Image src, NIVision.Image dest, double scaleX, double scaleY) {
+		NIVision.imaqScale(dest, src, ) //to be finished
+	}
+	public double[][] getGoalCenters() {
+		return null;
+	}
+	
+	//private NetworkTable table;
+	//private static final double[] defaultValue = {-20.0, -20.0, -20.0};
+	
+	/*public GoalFinder() {
+		table = NetworkTable.getTable("grip");
 		
 		try {
             new ProcessBuilder("/home/lvuser/grip").inheritIO().start();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-	}
+        }
+	}*/
 	
-	public double[][] getGoalCenters() {
-		/*double[] centerXs = table.getNumberArray("centerX", defaultValue);
+	/*public double[][] getGoalCenters() {
+		double[] centerXs = table.getNumberArray("centerX", defaultValue);
 		double[] centerYs = table.getNumberArray("centerX", defaultValue);
 		double[][] centers = new double[centerXs.length][2];
 		for(int a = 0; a < centers.length; a++) {
 			centers[a] = new double[]{centerXs[a], centerYs[a]};
 		}
 		
-		return centers;*/
+		return centers;
 		return null;
-	}
+	}*/
 }
