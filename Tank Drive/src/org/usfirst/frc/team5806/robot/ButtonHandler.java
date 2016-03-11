f03c950f6da30db0c63c6f1673625d04116fbcad
Drive Station <michael_truell@horacemann.org>
2016-03-11 13:21:04 -0500

    Triggers

--- Tank Drive/src/org/usfirst/frc/team5806/robot/ButtonHandler.java
+++ Tank Drive/src/org/usfirst/frc/team5806/robot/ButtonHandler.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 public class ButtonHandler {
 	Button[] buttons;
 	public ButtonHandler(Joystick joystick) {
-		buttons = new Button[]{new Button(joystick, 1), new Button(joystick, 3), new Button(joystick, 4), new Button(joystick, 2)};
+		buttons = new Button[]{new Button(joystick, 1), new Button(joystick, 3), new Button(joystick, 4), new Button(joystick, 2), new Button(joystick, 5), new Button(joystick, 6)};
 	}
 	
 	public Button getButtonWithTitle(char buttonLabel) {
@@ -13,6 +13,8 @@ public class ButtonHandler {
 		if(buttonLabel == 'X') return buttons[1];
 		if(buttonLabel == 'Y') return buttons[2];
 		if(buttonLabel == 'B') return buttons[3];
+		if(buttonLabel == 'L') return buttons[4];
+		if(buttonLabel == 'R') return buttons[5];
 		
 		return null;
 	}
